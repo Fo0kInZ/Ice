c48133556c4a28065415c5128c0bfed90877b33e
carrollcongress <louisjencka@gmail.com>
2011-10-15 15:58:56 -0600

    Initial commit

---
+++ Ice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ice
+//
+//  Created by louie on 10/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
